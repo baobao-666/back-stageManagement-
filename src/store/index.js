adfb98cbe4122e828b0200b8275c29da7b146cf2
baobao666 <1084403501@qq.com>
2019-12-19 16:58:22 +0800

    改bug

--- src/store/index.js
+++ src/store/index.js
@@ -19,18 +19,18 @@ import Tm  from './modules/TestQuestionManagement'
 
 Vue.use(Vuex)
 
-// // https://webpack.js.org/guides/dependency-management/#requirecontext
-// const modulesFiles = require.context('./modules', true, /\.js$/)
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+const modulesFiles = require.context('./modules', true, /\.js$/)
 
-// // you do not need `import app from './modules/app'`
-// // it will auto require all vuex module from modules file
-// const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-//   // set './app.js' => 'app'
-//   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-//   const value = modulesFiles(modulePath)
-//   modules[moduleName] = value.default
-//   return modules
-// }, {})
+// you do not need `import app from './modules/app'`
+// it will auto require all vuex module from modules file
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
 
 const store = new Vuex.Store({
   modules,
